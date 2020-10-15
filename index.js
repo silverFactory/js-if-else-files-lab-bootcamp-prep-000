125de0f6f16a503b96a1a2fed08776363e2917a9
Will Studabaker <will.studabaker@gmail.com>
2020-10-15 13:40:10 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,11 @@
+// ./js/index.js
+
+var name = "Joe"
+var height = "74"
+
+// Don't worry about this
+module.exports = { name, height
+}
+if (name === "Susan") {
+    console.log("The name is correct")
+}
